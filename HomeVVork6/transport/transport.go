3f9aeccb3d87a16ca7c0508b1fb8aacbba090fe9
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-03-26 01:21:20 +0200

    Update transport.go

--- HomeVVork6/transport/transport.go
+++ HomeVVork6/transport/transport.go
@@ -1,1 +1,37 @@
+package transport
+
+type PublicTransport interface {
+	BoardPassengers()    
+	DisembarkPassengers()
+}
+
+type Bus struct{}
+
+func (b Bus) BoardPassengers() {
+	println("Пасажири сідають у автобус")
+}
+
+func (b Bus) DisembarkPassengers() {
+	println("Пасажири виходять з автобуса")
+}
+
+type Train struct{}
+
+func (t Train) BoardPassengers() {
+	println("Пасажири сідають у потяг")
+}
+
+func (t Train) DisembarkPassengers() {
+	println("Пасажири виходять з потягу")
+}
+
+type Plane struct{}
+
+func (p Plane) BoardPassengers() {
+	println("Пасажири сідають у літак")
+}
+
+func (p Plane) DisembarkPassengers() {
+	println("Пасажири виходять з літаку")
+}
 
