d42e501a196979cf0e181a2d2d2b300de2a85252
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-03-26 01:22:03 +0200

    Update route.go

--- HomeVVork6/route/route.go
+++ HomeVVork6/route/route.go
@@ -1,1 +1,32 @@
+package route
+
+import "awesomeProject/transport" 
+
+type Route struct {
+	Transports []transport.PublicTransport 
+}
+
+func NewRoute() *Route {
+	return &Route{}
+}
+
+func (r *Route) AddTransport(t transport.PublicTransport) {
+	r.Transports = append(r.Transports, t)
+}
+
+func (r *Route) ShowTransports() {
+	for i, t := range r.Transports {
+		println("Транспортний засіб", i+1)
+		switch t.(type) {
+		case transport.Bus:
+			println("Тип: Автобус")
+		case transport.Train:
+			println("Тип: Потяг")
+		case transport.Plane:
+			println("Тип: Літак")
+		default:
+			println("Тип: Невідомий")
+		}
+	}
+}
 
