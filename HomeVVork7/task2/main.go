597d796001df1909be42420315effcab68d1767c
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-03-31 23:00:15 +0300

    Update main.go

--- HomeVVork7/task2/main.go
+++ HomeVVork7/task2/main.go
@@ -3,24 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
 func randomGenerator(min, max int, ch chan<- int) {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 10; i++ { 
+	for i := 0; i < 10; i++ {
 		randomNumber := rand.Intn(max-min+1) + min
 		ch <- randomNumber
 		time.Sleep(time.Second)
 	}
-	close(ch) 
+	close(ch)
 }
 
-func findMinMax(numbers <-chan int, result chan<- [2]int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	min := int(^uint(0) >> 1) 
-	max := int(^int(0) >> 1)  
+func findMinMax(numbers <-chan int, result chan<- [2]int) {
+	min := int(^uint(0) >> 1)
+	max := int(^int(0) >> 1)
 	for num := range numbers {
 		if num < min {
 			min = num
@@ -30,24 +28,16 @@ func findMinMax(numbers <-chan int, result chan<- [2]int, wg *sync.WaitGroup) {
 		}
 	}
 	result <- [2]int{min, max}
+	close(result)
 }
 
 func main() {
-	min, max := 1, 100 
+	min, max := 1, 100
 	numbers := make(chan int)
 	result := make(chan [2]int)
-	var wg sync.WaitGroup
 
-	wg.Add(1)
 	go randomGenerator(min, max, numbers)
-
-	wg.Add(1)
-	go findMinMax(numbers, result, &wg)
-
-	go func() {
-		wg.Wait()     
-		close(result) 
-	}()
+	go findMinMax(numbers, result)
 
 	minMax := <-result
 	fmt.Printf("Найменше число: %d, Найбільше число: %d\n", minMax[0], minMax[1])
