83a7905427d3a76d11a49a0b2d0823eb9fc6a3ec
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-04-04 01:34:21 +0300

    Update main.go

--- HomeVVork8/main.go
+++ HomeVVork8/main.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -19,38 +18,32 @@ type Round struct {
 
 type Player struct {
 	Name  string
-	Input chan int
 	Score int
 }
 
 func newPlayer(name string) *Player {
 	return &Player{
-		Name:  name,
-		Input: make(chan int),
+		Name: name,
 	}
 }
 
-func (p *Player) play(ctx context.Context, round Round, resultChan chan<- string) {
+func (p *Player) play(round Round) {
 	fmt.Printf("[%s] Question: %s\n", p.Name, round.Question)
 	for i, option := range round.Options {
 		fmt.Printf("[%s] Option %d: %s\n", p.Name, i+1, option)
 	}
 
-	select {
-	case ans := <-p.Input:
-		if ans == round.Answer {
-			fmt.Printf("[%s] Correct answer!\n", p.Name)
-			p.Score++
-		} else {
-			fmt.Printf("[%s] Wrong answer!\n", p.Name)
-		}
-	case <-ctx.Done():
-		fmt.Printf("[%s] Game ended!\n", p.Name)
-		resultChan <- fmt.Sprintf("[%s] Game ended!\n", p.Name)
+	// Просто для прикладу, гравець вибирає випадкову відповідь
+	ans := rand.Intn(len(round.Options)) + 1
+	if ans == round.Answer {
+		fmt.Printf("[%s] Correct answer!\n", p.Name)
+		p.Score++
+	} else {
+		fmt.Printf("[%s] Wrong answer!\n", p.Name)
 	}
 }
 
-func generateRound() (Round, Round, Round) {
+func generateRound() Round {
 	questions := []string{"Який футбольний клуб з Англії?", "Хто виграв Золотий мяч?", "У якому клубі грав Андрій Шевченко?"}
 	options := [][]string{
 		{"Челсі", "Реал Мадрид", "Шахтар", "Ювентус"},
@@ -59,33 +52,19 @@ func generateRound() (Round, Round, Round) {
 	}
 	answers := []int{1, 1, 2}
 
-	idx1 := rand.Intn(len(questions))
-	idx2 := (idx1 + 1) % len(questions)
-	idx3 := (idx1 + 2) % len(questions)
-
-	round1 := Round{
-		Question: questions[idx1],
-		Options:  options[idx1],
-		Answer:   answers[idx1],
+	idx := rand.Intn(len(questions))
+	return Round{
+		Question: questions[idx],
+		Options:  options[idx],
+		Answer:   answers[idx],
 	}
-	round2 := Round{
-		Question: questions[idx2],
-		Options:  options[idx2],
-		Answer:   answers[idx2],
-	}
-	round3 := Round{
-		Question: questions[idx3],
-		Options:  options[idx3],
-		Answer:   answers[idx3],
-	}
-
-	return round1, round2, round3
 }
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Обробка сигналів OS
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
@@ -94,56 +73,23 @@ func main() {
 		cancel()
 	}()
 
-	var wg sync.WaitGroup
-
-	rounds := make(chan Round)
-	resultChan := make(chan string)
- 
+	// Гравці
 	players := []*Player{
 		newPlayer("Player1"),
 		newPlayer("Player2"),
 		newPlayer("Player3"),
 	}
 
-	for _, player := range players {
-		wg.Add(1)
-		go func(p *Player) {
-			defer wg.Done()
-			for {
-				select {
-				case round := <-rounds:
-					p.play(ctx, round, resultChan)
-				case <-ctx.Done():
-					return
-				}
-			}
-		}(player)
-	}
-
-
+	// Генерація раундів
 	go func() {
 		ticker := time.NewTicker(5 * time.Second)
 		defer ticker.Stop()
 		for {
 			select {
 			case <-ticker.C:
-				round1, round2, round3 := generateRound()
-				for _ = range players {
-					select {
-					case rounds <- round1:
-					case <-ctx.Done():
-						return
-					}
-					select {
-					case rounds <- round2:
-					case <-ctx.Done():
-						return
-					}
-					select {
-					case rounds <- round3:
-					case <-ctx.Done():
-						return
-					}
+				round := generateRound()
+				for _, player := range players {
+					player.play(round)
 				}
 			case <-ctx.Done():
 				return
@@ -151,17 +97,8 @@ func main() {
 		}
 	}()
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case result := <-resultChan:
-				fmt.Println(result)
-			}
-		}
-	}()
-
-	wg.Wait()
+	<-ctx.Done() // Блокуємо головний потік доки не буде скасований контекст
 	fmt.Println("Гра завершена!")
 }
+
+
