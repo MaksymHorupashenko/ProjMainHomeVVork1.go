7a2d87ad67d141b47bc1bdc5aa3a4d5c8ca0a6e3
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-04-21 23:19:40 +0300

    Update main.go

--- HomeVVork11/task1/main.go
+++ HomeVVork11/task1/main.go
@@ -1,1 +1,25 @@
+package main
 
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("numbers.txt")
+	if err != nil {
+		fmt.Println("Помилка при зчитуванні файлу:", err)
+		return
+	}
+
+	phoneRegex := regexp.MustCompile(`(\+?(\d{1,3})[ -]?)?(\(?\d{3}\)?[ -]?)?\d{3}[ -]?\d{4}`)
+
+	phoneNumbers := phoneRegex.FindAllString(string(content), -1)
+
+	fmt.Println("Знайдені номери телефонів:")
+	for _, number := range phoneNumbers {
+		fmt.Println(number)
+	}
+}
