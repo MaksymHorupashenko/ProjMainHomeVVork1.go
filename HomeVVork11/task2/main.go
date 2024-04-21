5c47d546eec6df772ea531c221e59c64bdd84246
MaksymHorupashenko <162494836+MaksymHorupashenko@users.noreply.github.com>
2024-04-21 23:36:31 +0300

    Update main.go

--- HomeVVork11/task2/main.go
+++ HomeVVork11/task2/main.go
@@ -1,1 +1,26 @@
+package main
+
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("text.txt")
+	if err != nil {
+		fmt.Println("Помилка при зчитуванні файлу:", err)
+		return
+	}
+
+	wordRegex := regexp.MustCompile(`\b[a-zA-Z]*[aeiouAEIOU][a-zA-Z]*[^aeiouAEIOU\W]\b`)
+
+	words := wordRegex.FindAllString(string(content), -1)
+
+	fmt.Println("Знайдені слова, що починаються на голосну та закінчуються на приголосну:")
+	for _, word := range words {
+		fmt.Println(word)
+	}
+}
 
